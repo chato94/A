704172dc08cc290bbd77a851af09019fe57016a9
chato94 <chato94@gmail.com>
2015-08-23 17:55:34 -0500

    Added more basic functionality and renamed the LoadingIcon .step method to .tick as it is more of a tick in time than a step in a direction

--- init/js/loading.js
+++ init/js/loading.js
@@ -1,35 +1,45 @@
-var can = document.getElementById ('loading'), ctx = can.getContext ('2d');
+$(function () {
+    var can = document.getElementById ('loading'), ctx = can.getContext ('2d');
 
-var frame = 0, r0 = Math.min (can.width, can.height) * 0.4, r1 = 11 * r0 / 13;
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    
+    var frame = 0, c0 = 0.4, c1 = 11 / 13, r0 = Math.min (can.width, can.height) * c0, r1 = r0 * c1;
 
-(function animate () {
-    var deg = (function (x) {return -Math.PI * Math.cos (x / 50) + Math.PI;})(frame++);
-    ctx.clearRect (0, 0, can.width, can.height);
+    $(window).resize (function () {
+        ctx.canvas.width = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+        r0 = Math.min (can.width, can.height) * c0;
+        r1 = r0 * c1;
+    });
 
-    ctx.fillStyle = 'rbg(225, 225, 225)';
-    ctx.beginPath ();
-    ctx.arc (can.width / 2, can.height / 2, r0, 0, 2 * Math.PI, false);
-    ctx.arc (can.width / 2, can.height / 2, r1, 2 * Math.PI, 0, true);
-    ctx.fill ();
+    (function animate () {
+        var deg = (function (x) {return -Math.PI * Math.cos (x / 50) + Math.PI;})(frame++);
+        ctx.clearRect (0, 0, can.width, can.height);
 
-    ctx.fillStyle = 'green';
-    ctx.beginPath ();
-    ctx.arc (can.width / 2, can.height / 2, r0 - r1 * expF ((1 - Math.cos (deg)) / 2), mod (-Math.PI / 2, 2 * Math.PI), mod (deg - Math.PI / 2, 2 * Math.PI), false);
-    ctx.arc (can.width / 2, can.height / 2, r1 - r1 * expF ((1 - Math.cos (deg)) / 2), mod (deg - Math.PI / 2, 2 * Math.PI), mod (-Math.PI / 2, 2 * Math.PI), true);
-    ctx.fill ();
+        ctx.fillStyle = 'rbg(225, 225, 225)';
+        ctx.beginPath ();
+        ctx.arc (can.width / 2, can.height / 2, r0, 0, 2 * Math.PI, false);
+        ctx.arc (can.width / 2, can.height / 2, r1, 2 * Math.PI, 0, true);
+        ctx.fill ();
 
-    ctx.beginPath ();
-    ctx.fillStyle = 'rgb(225, 225, 225)';
-    ctx.arc (can.width / 2, can.height / 2, r0 - r1, 0, 2 * Math.PI, false);
-    ctx.fill ();
-    requestAnimationFrame (animate);
-})();
+        ctx.fillStyle = 'green';
+        ctx.beginPath ();
+        ctx.arc (can.width / 2, can.height / 2, r0 - r1 * expF ((1 - Math.cos (deg)) / 2), mod (-Math.PI / 2, 2 * Math.PI), mod (deg - Math.PI / 2, 2 * Math.PI), false);
+        ctx.arc (can.width / 2, can.height / 2, r1 - r1 * expF ((1 - Math.cos (deg)) / 2), mod (deg - Math.PI / 2, 2 * Math.PI), mod (-Math.PI / 2, 2 * Math.PI), true);
+        ctx.fill ();
 
-function expF (p) {var b = 5, c = 4; return (Math.pow(b, c * p - c) - Math.pow(b, -c)) / (1 - Math.pow(b, -c));}
+        ctx.beginPath ();
+        ctx.fillStyle = 'rgb(225, 225, 225)';
+        ctx.arc (can.width / 2, can.height / 2, r0 - r1, 0, 2 * Math.PI, false);
+        ctx.fill ();
+        requestAnimationFrame (animate);
+    })();
 
-function mod (x, y) {
-    return x < 0? y - (-x % y) : x % y;
-}
+    function expF (p) {var b = 5, c = 4; return (Math.pow(b, c * p - c) - Math.pow(b, -c)) / (1 - Math.pow(b, -c));}
+
+    function mod (x, y) {return x < 0? y - (-x % y) : x % y;}
+});
 
 /**
  * Creates a loading icon like the one at http://reddit.com/r/loadingicon/comments/293lqt/bored_at_work_so_look_what_i_made/
@@ -44,18 +54,27 @@ function mod (x, y) {
  *           outerColor - 'rgb([0, 255], [0, 255], [0, 255])': marks the color of the loading circumference
  *           radianDisplacement - [0, TAU): radian value added to the starting point of the loading animation along the circumference (mod TAU)
  *           radius     - [0, inf): specifies the radius of the outer circle of the loading icon
- *           rframes    - [0, inf): specifies the number of frames to animate the traveling sections
+ *           tFrames    - [0, inf): specifies the number of frames to animate the traveling sections
+ *           pEmTravFrames - [0, 1]: the percent of tFrames that are spent emerging from the center circle
  *           pRWidth    - [0, 1]: specifies the percentage of the radius that will be dedicated to loadColor (thickness of donut)
  */
 function LoadingIcon (canvas, opt) {
     var N_FRAMES = opt.glowFrames || 24, TAU = 2 * Math.PI,
 
-        lColor = opt.loadColor || 'rgb(0, 153, 0)', gColor = opt.glowColor || 'rgb(128, 255, 0)', oColor = opt.outerColor || 'rgb(200, 200, 200)',
-        globalColorTweener = new ColorTweener (lColor, gColor, N_FRAMES), nTF = opt.rframes || 20 /*numTravelingFrames*/,
-        can = canvas, ctx = can.getContext ('2d'), r = opt.radius || 0.25 * Math.min (can.width, can.height), r1 = r * (opt.pRWidth || 0.0625);
+        lColor = opt.loadColor || 'rgb(0, 153, 0)',
+        gColor = opt.glowColor || 'rgb(128, 255, 0)',
+        oColor = opt.outerColor || 'rgb(200, 200, 200)',
+        globalColorTweener = new ColorTweener (lColor, gColor, N_FRAMES),
+        nTF = opt.tFrames || 20, // number of traveling frames
+        pETF = opt.pEmTravFrames || 20, // percentage of frames used to emerge from the circle for traveling sections
+        nRF = opt.numRadFrames || 20, // number of frames for radiating sections
+        ctx = can.getContext ('2d'),
+        can = canvas,
+        r = opt.radius || 0.25 * Math.min (can.width, can.height),
+        r1 = r * (opt.pRWidth || 0.0625);
 
     // sects is for animating individual sections to the circumference, and per store the previous and current percentage values
-    var sects = [], pPrev = 0, pCurr = 0, rot = opt.radianDisplacement || -TAU / 4;
+    var sects = [], pPrev = 0, pCurr = 0, pDone = 0, rot = opt.radianDisplacement || -TAU / 4;
 
     // Animates the loading icon to the next stage. Note that it clears the context first
     this.draw = function (x, y) {
@@ -68,33 +87,32 @@ function LoadingIcon (canvas, opt) {
         // Draw the outer fill/container ring
         draw (1, oColor, 0, TAU, x, y);
 
-        // Draw the completed progress with loadColor
-        draw (1, lColor, 0, TAU * pPrev / 100, x, y);
+        // Draw the percent done (pDone) with loadColor (lColor)
+        draw (1, globalColorTweener.color (), mod (0 + rot, TAU), mod (ptoR (pDone) + rot, TAU), x, y);
 
         // Draw the transitioning sections
 
         // Draw the center circle and the text with the percent status
     };
 
+    // Returns whether the loading icon is at both 100% and finished animating
     this.done = function () {
-        var allSectsDone = true;
-        for (var i = 0; i < sects.length; i++) {
-            if (!sects[i].done ()) {
-                allSectsDone = false;
-                break;
-            }
-        }
-        return allSectsDone && pCurr === 100;
+        // Only need to check the last section as sections are synchronous
+        return sects.length && sects[sects.length - 1].done () && pCurr === 100;
     };
 
-    this.updatePercentage = function (quantity) {
-        // Percentage loaded can only go forward
-        pPrev = pCurr;
-        pCurr += Math.abs (+quantity);
-        if (pCurr > 100) pCurr = 100;
+    // Steps the loading icon one instance further in frame time
+    this.tick = function (quantity) {
+        // Add the absolute value of the quantity of percent, if any, to pCurr
+        if (arguments.length === 1) {
+            pPrev = pCurr;
+            pCurr += Math.abs(+quantity);
+            if (pCurr > 100) pCurr = 100;
 
-        // Create a new transitioning section based on the difference between pPrev and pCurr
-        sects.push (new TravelingSection ());
+            // Add the new transitioning segment based on the difference between pCurr and pPrev
+            sects.push (new TravelingSection (pToR (pPrev), pToR (pCurr), nTF, new ColorTweener (lColor, gColor, N_FRAMES), pETF, nRF));
+        }
+
         return this;
     };
 
@@ -116,6 +134,9 @@ function LoadingIcon (canvas, opt) {
 
     // Returns x % y like in Python
     function mod (x, y) {return x < 0? y - (-x % y) : x % y;}
+
+    // Converts a [0, 100] percentage to a radian value
+    function ptoR (percentage) {return TAU * percentage / 100;}
 
     /**
      * Handles properties of traveling sections like speed, color, and duration
@@ -197,16 +218,24 @@ function LoadingIcon (canvas, opt) {
     function RadiatingSection (theta0, theta1, numRadFrames, rPercentage, pThick) {
         var t0 = theta0, t1 = theta1, n = numRadFrames, pT = pThick, rPer = rPercentage, i = 0;
 
-        this.draw = function (x, y) {
-            draw (1 + e (i / n) * rPer, lColor, t0, t1, x, y, pT);
+        // Draws the radiating section to the canvas
+        //
+        // Arguments:
+        //     x, y   - (-inf, inf): location of the center from which to radiate
+        //     easing - function: function that defines the transition of a percentage from 0 to 1, inclusive
+        this.draw = function (x, y, easing) {
+            if (!easing) easing = function (percentThickness) {return percentThickness;};
+            draw (1 + e (i / n) * rPer, lColor, t0, t1, x, y, easing (pT));
             return this;
         };
 
+        // Steps the radiating animation one frame forward
         this.step = function () {
-            i++
+            i++;
             return this;
         };
 
+        // Returns whether the section is finished radiating or not
         this.done = function () {
             return i >= n;
         };
@@ -254,10 +283,12 @@ function LoadingIcon (canvas, opt) {
             return this;
         };
 
+        // Same as toString ()
         this.color = function () {
             return this.toString ();
         };
 
+        // Linearly interpolates two arrays of 3 values
         function interpolate (arr0, arr1, p) {
             var q = 1 - p;
             return [q * arr0[0] + p * arr1[0], q * arr0[1] + p * arr1[1], q * arr0[2] + p * arr1[2]];
@@ -341,11 +372,13 @@ function LoadingIcon (canvas, opt) {
             return [L, a, b];
         }
 
+        // Returns an array of 3 numeric values from an 'rgb(val1, val2, val3)' string
         function parse (rgbStr) {
             var strs = rgbStr.match (/\d+/g);
             return [+strs[0], +strs[1], +strs[2]];
         }
 
+        // Returns the 'rgb()' string of the current state of the tweener
         this.toString = function () {
             return 'rgb(' + Math.floor (cRGB[0]) + ', ' + Math.floor (cRGB[1]) + ', ' + Math.floor (cRGB[2]) + ')';
         };
